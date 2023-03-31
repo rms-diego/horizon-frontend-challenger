4e0a744f512a68e815640b88efd8f30ae044b8f8
Diego Ramos <diegorbrito9@gmail.com>
2023-03-31 16:15:25 -0300

    new: criando o roteamento basico da aplicação

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { App } from "./App";
+
+import { AppRoutes } from "./Routes";
+import { BrowserRouter } from "react-router-dom";
 
 import "./styles/globalStyles.scss";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
   </React.StrictMode>
 );
