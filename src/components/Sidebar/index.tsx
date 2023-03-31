0ede06f5f3bfe561c84f99fd4ce38dde4e5695dc
Diego Ramos <diegorbrito9@gmail.com>
2023-03-31 17:09:09 -0300

    new: criando o componente sidebar e seus estilos

---
+++ src/components/Sidebar/index.tsx
@@ -0,0 +1,44 @@
+import styles from "./styles.module.scss";
+
+import { FaHome, FaSearch, FaBook, FaSpotify } from "react-icons/fa";
+
+const categories = [
+  "Funk",
+  "Rock",
+  "Indie",
+  "Jazz",
+  "Bossa Nova",
+  "Pop",
+  "R&B",
+];
+
+export function Sidebar() {
+  return (
+    <aside className={styles.asideContainer}>
+      <FaSpotify size={35} className={styles.spotifyLogo} />
+      <div className={styles.asideNavigation}>
+        <p className={styles.notAllowed}>
+          <FaHome />
+          Home
+        </p>
+
+        <p className={styles.selected}>
+          <FaSearch />
+          Search
+        </p>
+
+        <p className={styles.notAllowed}>
+          <FaBook /> Library
+        </p>
+
+        <hr />
+      </div>
+
+      <div className={styles.categoriesList}>
+        {categories.map((category) => (
+          <p className={styles.notAllowed}>{category}</p>
+        ))}
+      </div>
+    </aside>
+  );
+}
